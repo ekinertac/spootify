6ba4712eb584addc230509ee1eaffb19ca00fef5
ekinertac <ekinertac@gmail.com>
2014-11-01 08:02:52 +0200

    resources

--- static/app/app.js
+++ static/app/app.js
@@ -1,32 +1,3 @@
-var Resources = {
-    playlists: [
-        {name: 'Tribute to Metallica', owner: 'Javi Salinas', id: 0},
-        {name: 'Solitary Ride', owner: 'Spotify', id: 1},
-        {name: 'Sekizbit', owner: 'Ekin Ertaç', id: 2},
-        {name: 'Adrelanline Workout', owner: 'Spotify', id: 3}
-    ],
-    playlist_detail: {
-        0: {
-            'artist': 'Various Artists',
-            'name': 'Tribute to Metallica',
-            'owner': {
-                'name': 'Javi Salinas',
-                'id': 2
-            },
-            'tracks': [
-                {title: 'Whiplash', artist: 'Motörhead', time: '4:25'},
-                {title: 'One', artist: 'Crematory', time: '4:25'},
-                {title: 'Damage, Inc.', artist: 'Floatsam & Jetsam', time: '4:25'},
-                {title: 'Holier Than Thou', artist: 'Testament', time: '4:25'},
-                {title: 'Master Of Puppets', artist: 'Ugly Kid Joe', time: '4:25'},
-                {title: 'Creeping Death', artist: 'Dark Angel', time: '4:25'},
-                {title: 'Eye of the Beholder', artist: 'In Flames', time: '4:25'}
-            ]
-        }
-    }
-};
-
-
 var Spotify = angular.module('Spotify', ['ngRoute']);
 
 Spotify.config(function ($interpolateProvider, $routeProvider) {
--- static/app/resource.js
+++ static/app/resource.js
@@ -1,22 +1,28 @@
-var playlists = [
-    {name: 'Tribute to Metallica', owner: 'Javi Salinas', id: 0},
-    {name: 'Solitary Ride', owner: 'Spotify', id: 1},
-    {name: 'Sekizbit', owner: 'Ekin Ertaç', id: 2},
-    {name: 'Adrelanline Workout', owner: 'Spotify', id: 3}
-];
-
-var playlist_detail = {
-    0: {
-        'artist': 'Various Artists',
-        'name': 'Tribute to Metallica',
-        'tracks': [
-            {track: 'Whiplash', artist: 'Motörhead', time: '4:25'},
-            {track: 'One', artist: 'Crematory', time: '4:25'},
-            {track: 'Damage, Inc.', artist: 'Floatsam & Jetsam', time: '4:25'},
-            {track: 'Holier Than Thou', artist: 'Testament', time: '4:25'},
-            {track: 'Master Of Puppets', artist: 'Ugly Kid Joe', time: '4:25'},
-            {track: 'Creeping Death', artist: 'Dark Angel', time: '4:25'},
-            {track: 'Eye of the Beholder', artist: 'In Flames', time: '4:25'}
-        ]
+var Resources = {
+    playlists: [
+        {name: 'Tribute to Metallica', owner: 'Javi Salinas', id: 0},
+        {name: 'Solitary Ride', owner: 'Spotify', id: 1},
+        {name: 'Sekizbit', owner: 'Ekin Ertaç', id: 2},
+        {name: 'Adrelanline Workout', owner: 'Spotify', id: 3}
+    ],
+    playlist_detail: {
+        0: {
+            'artist': 'Various Artists',
+            'name': 'Tribute to Metallica',
+            'owner': {
+                'name': 'Javi Salinas',
+                'id': 2
+            },
+            'tracks': [
+                {title: 'Whiplash', artist: 'Motörhead', time: '4:25'},
+                {title: 'One', artist: 'Crematory', time: '4:25'},
+                {title: 'Damage, Inc.', artist: 'Floatsam & Jetsam', time: '4:25'},
+                {title: 'Holier Than Thou', artist: 'Testament', time: '4:25'},
+                {title: 'Master Of Puppets', artist: 'Ugly Kid Joe', time: '4:25'},
+                {title: 'Creeping Death', artist: 'Dark Angel', time: '4:25'},
+                {title: 'Eye of the Beholder', artist: 'In Flames', time: '4:25'}
+            ]
+        }
     }
 };
+
